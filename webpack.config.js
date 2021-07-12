58112c36bf1cbfbd0a7e9c80982f24520f65c396
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-07-12 14:45:20 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const htmlWebpack = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
