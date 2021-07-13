00edc57a881789373f92524bdf23b15b2952b7ac
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-07-13 15:14:47 +0200

    Add drag event

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const list = document.querySelector('.list-data');
 const description = document.querySelector('.input');
 const save = document.querySelector('.sub');
 const listItem = document.createElement('li');
+const icon = document.createElement('i');
+const span = document.createElement('span');
 const checkbox = document.createElement('checkbox');
+checkbox.classList.add('check');
+listItem.draggable = true;
 listItem.classList.add('list-item');
+list.appendChild(checkbox);
 list.appendChild(listItem);
 const todos = [
   {
@@ -15,15 +20,18 @@ const todos = [
   description: 'drink water',
   completed: true,
   index: 1
+  },
+  {
+    description: 'coding',
+    completed: false,
+    index: 2
   }
 ]
 
 const addTodo = () => {
   const description = document.querySelector('.input').value;
   todos.push(description);
-  todos.forEach((item) => {
-    listItem.textContent = `${item.description}`;
-  })
+  listItem.innerHTML = `${description}<span class="span"><i class="fas fa-trash delete"></i></span`;
 } 
 
 const clearCompleted = () => {
