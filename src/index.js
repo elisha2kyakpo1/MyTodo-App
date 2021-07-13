7f293c6461aac5fe18904121ece7f67699d12893
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-07-13 09:58:21 +0200

    Add HTML-webpack plugin

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const list = document.querySelector('.list');
+
+import todo from "./todo";
+
+console.log(todo(3, 4));
+
+// console.log(list(3));
---
+++ src/todo.js
@@ -0,0 +1,9 @@
+const todo = (num1, num2) => {
+  return num1 + num2;
+}
+
+const list = (value) => {
+  return value;
+}
+
+export default todo ;
