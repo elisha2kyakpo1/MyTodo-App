0a7c18a88349f3cb4453766ae7a38e930de16a56
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-07-22 16:50:37 +0200

    Add displayTasks

--- test/display.test.js
+++ test/display.test.js
@@ -1,5 +1,15 @@
-import { displayTasks } from "../_mock_/updateTodo";
+import { expect } from "@jest/globals";
+import { displayTasks, tempTasks } from "../_mock_/updateTodo";
 
 test('displays the todos', () => {
-  
+  const obj = {
+    description: 'my data',
+    completed: true,
+    index: 0,
+  };
+  tempTasks.push(obj);
+  tempTasks.forEach((ele) => {
+    displayTasks();
+    expect(ele.description).toBe('my data');
+  })
 });
